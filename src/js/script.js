b5ce7b376534c04416d486d54f58885584e324b1
Akhmadi <ahmadi98ahmadi@gmail.com>
2021-02-19 20:47:41 +0600

    some fixes, mobile adaptation(modal windows)

--- src/js/script.js
+++ src/js/script.js
@@ -39,17 +39,23 @@ $(document).ready(function () {
 
   // Modal
 
-  $('[data-modal=consultation').on('click', function() {
+  $('[data-modal=consultation').on('click', function () {
     $('.overlay, #consultation').fadeIn();
 
   });
 
-  $('.modal__close').on('click', function() {
+  $('.modal__close').on('click', function () {
     $('.overlay, #consultation, #order, #thanks').fadeOut();
   });
 
-  $('.button_mini').each(function(i) {
-    $(this).on('click', function() {
+  $(window).on('click', function (e) {
+    if (e.target.classList.contains('overlay')) {
+      $('.overlay, #consultation, #thanks, #order').fadeOut();
+    }
+  });
+
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
       $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
       $('.overlay, #order').fadeIn();
     });
